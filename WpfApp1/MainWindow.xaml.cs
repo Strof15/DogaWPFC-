30eb88d6b7b5fb3ba3ada05c35900b95153febd0
Kristof <Kristof@DESKTOP-C9STJNN>
2023-05-19 13:39:35 +0200

    ennyi

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,73 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        static List<classszeleromu> szeleromuadatok = new List<classszeleromu>(); 
         public MainWindow()
         {
             InitializeComponent();
+            Beolvasas();
+            
+        }
+
+        static void Beolvasas()
+        {
+
+            StreamReader olvas = new StreamReader("szeleromu.txt");
+            olvas.ReadLine();
+            while (!olvas.EndOfStream)
+            {
+                string[] adat = olvas.ReadLine().Split(';');
+
+                szeleromuadatok.Add(
+                    adat[0], int.Parse(adat[1]), int.Parse(adat[2]), int.Parse(adat[3]));
+            }
+
+            
+
+            olvas.Close();
+            dataGridSzeleromu.ItemsSource = szeleromuadatok;
+
+
+        }
+
+
+        private void Btnegysegszam_Click(object sender, RoutedEventArgs e)
+        {
+            //lblegysegszam.Content =
+        }
+
+        private void Btnosszes_Click(object sender, RoutedEventArgs e)
+        {
+           lblosszes.Content = szeleromuadatok.Count();
+        }
+
+        private void Btnkategoria_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Btnatlagteljesitmeny_Click(object sender, RoutedEventArgs e)
+        {
+            lblatlagteljesitmeny.Content = Math.Round(szeleromuadatok.Average()[2], 2) ;
+
+        }
+
+        private void Btnlegnagyobbteljesitmeny_Click(object sender, RoutedEventArgs e)
+        {
+            lbllegnagyobbteljesitmeny.Content = szeleromuadatok.Max();
+        }
+
+        private void Btnjelentes_Click(object sender, RoutedEventArgs e)
+        {
+           // lbljelentes.Content =
+
+        }
+
+
+        private void lbadatok(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
+
 }
--- WpfApp1/classszeleromu.cs
+++ WpfApp1/classszeleromu.cs
@@ -9,8 +9,42 @@ namespace WpfApp1
     internal class classszeleromu
     {
 
+        //Az erőmű helyszíne, mennyi egységből áll, teljesítménye, és a működés kezdetének éve.
+
+        string helyszin;
+        int egysegekszama;
+        int teljesitemyn;
+        int evszam;
+
+        public classszeleromu(string helyszin, int egysegekszama, int teljesitemyn, int evszam)
+        {
+            this.helyszin = helyszin;
+            this.egysegekszama = egysegekszama;
+            this.teljesitemyn = teljesitemyn;
+            this.evszam = evszam;
+        }
+
+        public string Helyszin { get => helyszin; set => helyszin = value; }
+        public int Egysegekszama { get => egysegekszama; set => egysegekszama = value; }
+        public int Teljesitemyn { get => teljesitemyn; set => teljesitemyn = value; }
+        public int Evszam { get => evszam; set => evszam = value; }
+
+        public char Kategoria()
+        {
+            if (teljesitemyn >= 1000)
+            {
+                return 'A';
+            }
+            else if (teljesitemyn >= 500 && teljesitemyn < 1000)
+            {
+                return 'B';
+            }
+            else
+            {
+                return 'C';
+            }
 
 
 
-    }
+        }
 }
